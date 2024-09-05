ffc6954b01e65abc5843039e289944d2c65eb1cf
Kolchin13D <kolchin13d@gmail.com>
2024-09-05 12:39:51 +0300

    testing

--- app/src/main/java/com/example/tablayoutapp/MainActivity.kt
+++ app/src/main/java/com/example/tablayoutapp/MainActivity.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.example.tablayoutapp.adapter.viewAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    val tabsArray = arrayOf("Frag1", "Frag2", "Frag3")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val viewPager2 = findViewById<ViewPager2>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
+        val myAdapter = viewAdapter(
+            supportFragmentManager,
+            lifecycle
+        )
+
+        viewPager2.adapter = myAdapter
+
+        TabLayoutMediator(tabLayout, viewPager2){
+            tabLayout, position -> tabLayout.text = tabsArray[position]
+        }.attach()
+
     }
 }
